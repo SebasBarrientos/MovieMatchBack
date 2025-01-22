78c3029a0201063c2303663770595e8d578e5020
Sebastian <sebas33696@gmail.com>
2025-01-22 13:43:26 +0100

    cambio de url para deploy cors

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const { PORT } = process.env;
 
 const io = new Server(PORT, {
     cors: {
-        origin: "http://localhost:3001", // Reemplaza con la URL de tu frontend
+        origin: "http://localhost:3000", // Reemplaza con la URL de tu frontend
         methods: ["GET", "POST"],
     },
 });
